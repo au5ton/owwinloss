8d3bfe8fb256ff0e20a2e645ee551211c0e528c5
au5ton <au5ton@users.noreply.github.com>
2020-05-02 10:18:22 +0000

    deploy: f92f9aca42eb875856d842ad90a9556566816831

---
+++ precache-manifest.79a0893e1440e39053ef9871c4036cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bee143ab1c208e19c9cdb874c2961a",
+    "url": "/owwinloss/index.html"
+  },
+  {
+    "revision": "9e514a3756c262403661",
+    "url": "/owwinloss/static/css/main.72f21bb8.chunk.css"
+  },
+  {
+    "revision": "a5f5cd6923989bb27bf4",
+    "url": "/owwinloss/static/js/2.50ba3b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/owwinloss/static/js/2.50ba3b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e514a3756c262403661",
+    "url": "/owwinloss/static/js/main.4375c11e.chunk.js"
+  },
+  {
+    "revision": "65def22c938bedaf0cf7",
+    "url": "/owwinloss/static/js/runtime-main.e4763f34.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5f0c3d8137e8adb9a8f365bb06a8e31.js"
+  "/owwinloss/precache-manifest.79a0893e1440e39053ef9871c4036cdd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/owwinloss/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
